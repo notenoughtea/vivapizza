68e8e16905b27a3681ac77079bc2f41c53a40588
Alan <dreamwalk@mail.ru>
2022-07-01 22:00:02 +0300

    добавил post метод для создания карточек в базе

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -12,6 +12,23 @@ router
     } catch (err) {
       res.status(500);
     }
-  });
+  }
+  .post(async (req, res, next) => {
+      try {
+          if (req.body) {
+              const newRoute = await Card.create({
+                  title: req.body.title,
+                  price: req.body.price,
+                  text: req.body.text,
+                  // url: url + "/img/" + req.file.filename,
+              });
+              res.status(200).json(newRoute);
+          }
+      } catch (err) {
+          res.status(500);
+      }
+  }
+
+);
 
 module.exports = router;
